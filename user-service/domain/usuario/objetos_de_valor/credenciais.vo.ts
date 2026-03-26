c11a88c797b9193adb32b1be4f8588d0da6ef039
Guilherme <guittrex.2005@gmail.com>
2026-03-26 15:00:40 -0300

    fix: arrumando diretórios e criando .dockerignore para o projeto

--- user-service/domain/usuario/objetos_de_valor/credenciais.vo.ts
+++
@@ -1,9 +0,0 @@
-//VO responsável por armazenar as credenciais do usuário.
-export class Credenciais{
-  constructor(
-    public readonly email: string,
-    public readonly senhaHash: string){
-      if(!email.includes('@')) throw new Error('Email inválido.');
-      if(!senhaHash) throw new Error('Senha hash é obrigatória.');
-    }
-}
